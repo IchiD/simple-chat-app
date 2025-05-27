24b27db52fea3520452405e9c314f3832febe725
IchiD <bigpoem33@gmail.com>
2025-05-27 18:23:22 +0900

    admin

--- backend/app/Http/Middleware/AdminMiddleware.php
+++ backend/app/Http/Middleware/AdminMiddleware.php
@@ -8,19 +8,19 @@ use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next)
-    {
-        if (!Auth::guard('admin')->check()) {
-            return redirect()->route('admin.login');
-        }
-
-        return $next($request);
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @return mixed
+   */
+  public function handle(Request $request, Closure $next)
+  {
+    if (!Auth::guard('admin')->check()) {
+      return redirect()->route('admin.login');
     }
+
+    return $next($request);
+  }
 }
